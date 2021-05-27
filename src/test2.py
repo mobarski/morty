204144134e3e3214e6374cd58c747a2604b43a9e
Maciej Obarski <mobarski@gmail.com>
2021-05-27 22:07:37 +0200

    asm comments in test code

--- src/test2.py
+++ src/test2.py
@@ -4,24 +4,26 @@ import asm
 op_names = "stop _ call ret jz push _ mul add".split(' ')
 
 code1 = asm.to_cells("""
-	push 5
-	push 15
-	add 0
-	push 2
-	mul 0
-	push 1
-	push 1
-	add 0
-	add 0
+	push 5		(5)
+	push 15		(5 15)
+	add 0		(20)
+	push 2		(20 2)
+	mul 0		(40)
+	push 1		(40 1)
+	push 1		(40 1 1)
+	add 0		(40 2)
+	add 0		(42)
 	stop 0
 """, op_names)
 
+
 if __name__=="__main__":
+
 	vm = Test1VM(op_names)
 	vm.s = 0
 	vm.r = vm.f = 10
 	vm.code = code1 # <---[ SELECT CODE HERE ]
-	# --------------------------
+	
 	for i in range(20):
 		name,op,arg = vm.tick()
 		print(f"T:{vm.t}")
@@ -30,4 +32,3 @@ if __name__=="__main__":
 		print(name,arg)
 		print()
 		if op==0 and arg==0: break
-	
