f70e86f7fc8cb0af07a8f17abd60ea78e5b89ac6
Maciej Obarski <mobarski@gmail.com>
2021-05-27 21:51:16 +0200

    asm: simple error detection

--- src/asm.py
+++ src/asm.py
@@ -1,15 +1,15 @@
 import re
 
+# TODO map token to file @ line
+
 def to_cells(text, op_names):
 	op_code = {n:i for i,n in enumerate(op_names)}
-	text = strip_comments(text)
-	tokens = tokenize(text)
-	cells = precompile(tokens, op_code)
-	cells,labels = strip_labels(cells)
-	# TODO detect errors
-	cells = apply_labels(cells, labels)
+	text          = strip_comments(text)
+	tokens        = tokenize(text)
+	tokens,labels = strip_labels(tokens)
+	tokens        = apply_labels(tokens, labels)
+	cells         = compile(tokens, op_code)
 	return cells
-
 
 # -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
@@ -21,47 +21,44 @@ def strip_comments(text):
 def tokenize(text):
 	return re.split('\s+',text.strip())
 
-def precompile(tokens, op_code):
-	cells = []
-	for t in tokens:
-		c = op_code.get(t)
-		if c is not None:
-			cells += [c]
-		elif t[0]=='@':
-			cells += [t]
-		elif t[-1]==':':
-			cells += [t]
-		else:
-			cells += [int(t)]
-	return cells
-
-def strip_labels(cells):
+def strip_labels(tokens):
 	labels = {}
 	out = []
-	for c in cells:
-		if type(c) is int:
-			out += [c]
-		elif c[-1]==':':
-			name = c[:-1]
+	for t in tokens:
+		if t[-1]==':':
+			name = t[:-1]
 			labels[name] = len(out)
 		else:
-			out += [c]
+			out += [t]
 		
 	return out, labels
 
-def apply_labels(cells, labels):
+def apply_labels(tokens, labels):
 	out = []
-	for c in cells:
-		if type(c) is int:
-			out += [c]
-		elif c[:2] in ['@+','@-']:
-			out += [len(out) + int(c[1:])]
-		elif c[0]=='@':
-			name = c[1:]
+	for t in tokens:
+		if t[0]=='@':
+			name = t[1:]
 			out += [labels[name]]
+		elif t[:2] in ['@+','@-']:
+			out += [len(out) + int(t[1:])]
 		else:
-			raise Exception(f"unknown token: {c}")
+			out += [t]
 	return out
+
+def compile(tokens, op_code):
+	cells = []
+	for i,t in enumerate(tokens):
+		c = op_code.get(t)
+		if c is not None:
+			cells += [c]
+		else:
+			if i%2==0:
+				N = 5
+				info = ' '.join([str(x) for x in tokens[i-N:i]]) + f" >>>{t}<<< " + ' '.join([str(x) for x in tokens[i+1:i+N+1]])
+				print(f"ERROR: invalid token '{t}' at position {i}:\n{info}\n")
+				raise Exception('invalid syntax')
+			cells += [int(t)]
+	return cells
 
 # -----------------------------------------------------------------------------
 
@@ -82,7 +79,7 @@ if __name__=="__main__":
 			jz		@start
 		
 		mul4:
-			push 2
+			push 2 0
 			push 2
 			mul 0
 			mul 0
