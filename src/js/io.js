b7c687abbff9aa43184d4441372cb8df7fb9a9b1
Maciej Obarski <mobarski@gmail.com>
2021-07-16 18:21:16 +0200

    screen module prototyping

--- src/js/io.js
+++ src/js/io.js
@@ -16,24 +16,28 @@ function io_set(dev) {
 function screen_get() {
 	fun = s_pop()
 	switch (fun) {
-		case 3: s_push(scr.c);                     break; // scr-get-c
-		case 4: s_push(scr.i);                     break; // scr-get-i
-		case 5: s_push(0);                         break; // TODO: scr-get (pixel-color)
-		case 6: s_push(scr.w);                     break; // scr-get-w
-		case 7: s_push(scr.h);                     break; // scr-get-h
+		case 1: s_push(scr.w);                     break;
+		case 2: s_push(scr.h);                     break;
+		case 3: s_push(scr.pal.length/4);          break;
+		case 4: s_push(scr.c);                     break;
+		case 5: s_push(scr.i);                     break;
+		//...
+		case 13: s_push(pget()); scr.i++;          break;
 	}
 }
 
 function screen_set() {
 	fun = s_pop()
 	switch (fun) {
-		case 0: init_screen(scr.w, scr.h); break; // scr-init
-		case 1: flip();                    break; // scr-flip
-		case 2: cls(scr.c);                break; // scr-cls
-		case 3: scr.c = s_pop();           break; // scr-set-c
-		case 4: scr.i = s_pop();           break; // scr-set-i
-		case 5: pset(scr.i++);             break; // scr-put
-		case 6: scr.w = s_pop();           break; // scr-set-w
-		case 7: scr.h = s_pop();           break; // scr-set-h
+		case 1: scr.w = s_pop();           break; // scr-set-w
+		case 2: scr.h = s_pop();           break; // scr-set-h
+		case 3: break; // TODO: set palete length
+		case 4: scr.c = s_pop();           break; // scr-set-c
+		case 5: scr.i = s_pop();           break; // scr-set-i
+		//...
+		case 10: n=s_pop(); fill(scr.i, n, scr.c); scr.i+=n; break;
+		case 11: init_screen(scr.w, scr.h); break; // scr-init
+		case 12: flip();                    break; // scr-flip
+		case 13: pset(scr.i++);             break; // scr-put
 	}
 }
