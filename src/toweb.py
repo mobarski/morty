d1f48d67b26b85d4513859f6723dfe68202a988d
Maciej Obarski <mobarski@gmail.com>
2021-07-20 20:55:56 +0200

    js - embedding dependencies

--- src/toweb.py
+++ src/toweb.py
@@ -18,21 +18,30 @@ if __name__=="__main__":
 	parser.add_argument('-o',  metavar='path', type=str, help='output path (default: stdout)')
 	parser.add_argument('-f',  metavar='path', type=str, help='input path (default: stdin)')
 	parser.add_argument('-d',  action='store_true', help='debug')
+	parser.add_argument('-e',  action='store_true', help='embed dependencies')
 	args = parser.parse_args()
 
 	if args.f:
 		code = open(args.f,'r').read()
 	else:
 		code = sys.stdin.read()		
-		
+	
+	if args.e:
+		dependencies = "<script>\n"
+		dependencies += open('js/vm.js').read()
+		dependencies += open('js/io.js').read()
+		dependencies += open('js/screen.js').read()
+		dependencies += "</script>\n"
+	else:
+		dependencies = """
+			<script src="vm.js"></script>
+			<script src="io.js"></script>
+			<script src="screen.js"></script>
+		"""
 	tokens = tokenize(code)
 	rom = serialize(tokens)
-	html = f"""
-		<script src="vm.js"></script>
-		<script src="io.js"></script>
-
+	html = f"""{dependencies}
 		<div id="screen"></div>
-		<script src="screen.js"></script>
 		<a onclick='fullscreen()'>fullscreen</a/>
 
 		<script>
